00eeb9643299c364a3a51f5e9514f8a0a926b6bc
Conor <conor.cadogan@gmail.com>
2026-01-23 17:54:57 +0800

    fix: install gitconfig

--- scripts/bootstrap/git-config.sh
+++ scripts/bootstrap/git-config.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 setup_git_include() {
+  GIT_CONFIG="$HOME/.gitconfig"
+  DOTFILES_GITCONFIG="$HOME/.config/git/.gitconfig"
 
-  GIT_CONFIG = "$HOME/.gitconfig"
-  DOTFILES_GITCONFIG = "$HOME/.config/git/.gitconfig"
-
-  if [[ -f $GIT_CONFIG]]; then
-    if ! grep -q "\[include\]" $GIT_CONFIG || ! grep -q "path = $DOTFILES_GITCONFIG" "$GIT_CONFIG"; then
-        {
+  if [[ -f "$GIT_CONFIG" ]]; then
+    if ! grep -qF "[include]" "$GIT_CONFIG" || ! grep -qF "path = $DOTFILES_GITCONFIG" "$GIT_CONFIG"; then
+      {
         echo ""
         echo "[include]"
         echo "    path = $DOTFILES_GITCONFIG"
-        } >> "$GIT_CONFIG"
+      } >> "$GIT_CONFIG"
     fi
+  fi
 }
 
 setup_git_include
