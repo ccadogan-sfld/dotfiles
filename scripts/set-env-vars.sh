3e7c9c6fdead49b70b08f4c5e23661b5443bfcc3
Conor Cadogan <ccadogan@schonfeld.com>
2026-02-11 02:21:07 +0000

    feat: add ssh key placement

--- scripts/set-env-vars.sh
+++ scripts/set-env-vars.sh
@@ -2,3 +2,6 @@
 set -e
 
 aws secretsmanager get-secret-value --secret-id coder/ccadogan | jq -r '.SecretString | gsub("\r\n"; "\n")' > $HOME/.env.local
+
+aws secretsmanager get-secret-value --secret-id coder/ccadogan/keys | jq -r .SecretString | jq -r ".id_ed25519" | sed 's/\\n/\n/g' > $HOME/.ssh/id_ed25519
+aws secretsmanager get-secret-value --secret-id coder/ccadogan/keys | jq -r .SecretString | jq -r ".id_ed25519_pub" | sed 's/\\n/\n/g' > $HOME/.ssh/id_ed25519.pub
