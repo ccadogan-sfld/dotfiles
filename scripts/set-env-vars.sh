88aee529e67d22c40d3cc15ec0b5ec77715023bb
Conor Cadogan <ccadogan@schonfeld.com>
2026-03-10 04:42:47 +0000

    chore: check if .ssh directory exists

--- scripts/set-env-vars.sh
+++ scripts/set-env-vars.sh
@@ -3,5 +3,9 @@ set -e
 
 aws secretsmanager get-secret-value --secret-id coder/ccadogan | jq -r '.SecretString | gsub("\r\n"; "\n")' > $HOME/.env.local
 
+if [[ ! -d $HOME/.ssh ]]; then
+    mkdir $HOME/.ssh
+fi
+
 aws secretsmanager get-secret-value --secret-id coder/ccadogan/keys | jq -r .SecretString | jq -r ".id_ed25519" | sed 's/\\n/\n/g' > $HOME/.ssh/id_ed25519
 aws secretsmanager get-secret-value --secret-id coder/ccadogan/keys | jq -r .SecretString | jq -r ".id_ed25519_pub" | sed 's/\\n/\n/g' > $HOME/.ssh/id_ed25519.pub
